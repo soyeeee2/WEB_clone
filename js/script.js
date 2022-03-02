ef850ed497e74ac8ce82c1f1bc4fbce19265bb14
soyeeee2 <soyeeee2@gmail.com>
2022-03-02 14:57:47 +0900

    흐음

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@ $(document).ready(function () {
 
     $(".benefit-slider").slick({
       dots: true,
-    
+      prevArrow:'<i class="prev-arrow fa-solid fa-angle-left"></i>',
+      nextArrow:'<i class="next-arrow fa-solid fa-angle-right"></i>',
     });
   });
   
