1af3cc79f669ae84d98c8fb11d24554fe7980cfb
soyeeee2 <soyeeee2@gmail.com>
2022-03-02 20:20:07 +0900

    abc

--- js/script.js
+++ js/script.js
@@ -15,5 +15,16 @@ $(document).ready(function () {
       prevArrow:'<i class="prev-arrow fa-solid fa-angle-left"></i>',
       nextArrow:'<i class="next-arrow fa-solid fa-angle-right"></i>',
     });
+
+    // $(".sec-trend .hashtag").click(function(){
+    //   var indexNum = $(this).index();
+
+    //   $(this).addClass("active")
+    //   $(this).siblings().removeClass("active")
+
+    //   $(.sec-trend .content).eq(indexNum).addClass("active")
+    //   $(.sec-trend .content).eq(indexNum).siblings().removeClass("active")
+    // });
+
   });
   
