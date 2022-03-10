d0f7be930ae9fc9e44ded67e6ac0d4ac3b84c0a7
soyeeee2 <soyeeee2@gmail.com>
2022-03-10 22:26:51 +0900

    red

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
   $(".main-slider").slick({
+    fade: true,
+    autoplay: true,
   });
+
+  $('.main-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $(".main-slider-wrap .dots").eq(nextSlide).addClass("active")
+    $(".main-slider-wrap .dots").eq(nextSlide).siblings().removeClass("active")
+  });
+
+  $(".pro-slider").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: false,
+          dots: true
+        }
+      }
+    ],
+
+  });
+
 });
