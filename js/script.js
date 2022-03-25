10ba021465800bf5e24a2aceabf7c0d45021f0a3
soyeeee2 <soyeeee2@gmail.com>
2022-03-25 14:27:04 +0900

    slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
   $(".main-slider").slick({
     dots: false,
+    prevArrow: '<i class="fa-solid fa-chevron-left"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right"></i>',
+    autoplay: true,
+    asNavFor: ".sub-slider",
+  });
+  $(".sub-slider").slick({
+    dots: false,
+    prevArrow: '<i class="fa-solid fa-chevron-left"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right"></i>',
+    autoplay: true,
+    asNavFor: ".main-slider",
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
   });
 });
